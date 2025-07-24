b6839f0951ebc73226b0cb363150039a07e7d60f
maher-alhomsy <maheralhomsy2002@gmail.com>
2025-07-24 22:30:19 +0300

    fix issues inside getAllTokenAccounts util

--- utils/getAllTokenAccounts.js
+++ utils/getAllTokenAccounts.js
@@ -1,14 +1,19 @@
 import bs58 from 'bs58';
+import dotenv from 'dotenv';
 import { Keypair, PublicKey } from '@solana/web3.js';
 
-import connection from './connection';
+import connection from './connection.js';
+
+dotenv.config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY_BASE58;
 
 const secretKey = Keypair.fromSecretKey(bs58.decode(PRIVATE_KEY));
-const pubkey = secretKey.publicKey.toBase58();
+const _pubkey = secretKey.publicKey.toBase58();
 
 async function getAllTokenAccounts() {
+  const pubkey = new PublicKey(_pubkey);
+
   const tokenAccounts = await connection.getParsedTokenAccountsByOwner(pubkey, {
     programId: new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'),
   });
